e9fe80a6eff83a2b828c31a930ec581cf0275f9e
balshy <balshy@techsquadeg.com>
2021-05-22 03:25:27 +0200

    move login and register to resolvers file

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,64 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const db = require("../queries");
+
+const SECRETKEY = "SECRET KEY ";
 
 const getPostById = async (user_id) => {
   const user = await db.getUserById(user_id);
   return user;
 };
 
+const login = async (email, password) => {
+  const users = await db.getUserByEmail(email);
+  const user = users[0];
+  if (!user) {
+    throw new Error("This Email not registered in our database");
+  }
+  const correctPassword = await bcrypt.compareSync(password, user.password);
+  if (!correctPassword) {
+    throw new Error("Wrong password");
+  }
+
+  const token = await jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRETKEY
+  );
+  delete user.password;
+  user.token = token;
+  return user;
+};
+
+const register = async (email, password, confirmPassword, username) => {
+  if (password !== confirmPassword) {
+    throw new Error("password not match");
+  }
+  const users = await db.getUserByEmail(email);
+  if (users.length > 0) {
+    throw new Error("this email is taken");
+  }
+  const user = await db.addUser(email, password, username);
+
+  const token = await jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRETKEY
+  );
+  delete user.password;
+  user.token = token;
+  return user;
+};
+
 module.exports = {
   getPostById,
+  login,
+  register,
 };
--- graphql/shcema.js
+++ graphql/shcema.js
@@ -17,63 +17,9 @@ const SECRETKEY = "SECRET KEY ";
 
 const isAuth = require("../middlewares/isAuth");
 
-const { getPostById } = require("./resolvers");
+const { getPostById, login, register } = require("./resolvers");
 
-const PostType = new GraphQLObjectType({
-  name: "Post",
-  fields: () => ({
-    id: { type: GraphQLID },
-    user: {
-      type: UserType,
-      async resolve(parent, args) {
-        return await db.getUserById(parent.user_id);
-      },
-    },
-    body: { type: new GraphQLNonNull(GraphQLString) },
-    created_at: { type: new GraphQLNonNull(GraphQLString) },
-    likes: { type: new GraphQLNonNull(GraphQLInt) },
-    comments: {
-      type: new GraphQLList(CommentType),
-      async resolve(parent, args) {
-        return await db.getCommentsByPost(parent.id);
-      },
-    },
-  }),
-});
-
-const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    id: { type: GraphQLID },
-    username: { type: new GraphQLNonNull(GraphQLString) },
-    email: { type: new GraphQLNonNull(GraphQLString) },
-    password: { type: new GraphQLNonNull(GraphQLString) },
-    created_at: { type: new GraphQLNonNull(GraphQLString) },
-    token: { type: GraphQLString },
-  }),
-});
-
-const CommentType = new GraphQLObjectType({
-  name: "Comment",
-  fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLID) },
-    user: {
-      type: UserType,
-      async resolve(parent, args) {
-        return await db.getUserById(parent.user_id);
-      },
-    },
-    post: {
-      type: PostType,
-      async resolve(parent, args) {
-        const post = await db.getPostById(parent.post_id);
-        return post;
-      },
-    },
-    body: { type: new GraphQLNonNull(GraphQLString) },
-    created_at: { type: new GraphQLNonNull(GraphQLString) },
-  }),
-});
+const { PostType, UserType, CommentType } = require("./types");
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -119,25 +65,7 @@ const Mutation = new GraphQLObjectType({
       },
       async resolve(parent, args, context) {
         const { email, password, confirmPassword, username } = args;
-        if (password !== confirmPassword) {
-          throw new Error("password not match");
-        }
-        const users = await db.getUserByEmail(email);
-        if (users.length > 0) {
-          throw new Error("this email is taken");
-        }
-        const user = await db.addUser(email, password, username);
-
-        const token = await jwt.sign(
-          {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-          },
-          SECRETKEY
-        );
-        delete user.password;
-        user.token = token;
+        const user = await register(email, password, confirmPassword, username);
         return user;
       },
     },
@@ -150,29 +78,7 @@ const Mutation = new GraphQLObjectType({
 
       async resolve(parent, args) {
         const { email, password } = args;
-        const users = await db.getUserByEmail(email);
-        const user = users[0];
-        if (!user) {
-          throw new Error("This Email not registered in our database");
-        }
-        const correctPassword = await bcrypt.compareSync(
-          password,
-          user.password
-        );
-        if (!correctPassword) {
-          throw new Error("Wrong password");
-        }
-
-        const token = await jwt.sign(
-          {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-          },
-          SECRETKEY
-        );
-        delete user.password;
-        user.token = token;
+        const user = await login(email, password);
         return user;
       },
     },
