825db5875abe675a230db026a2dc58476f1ff0b3
balshy <balshy@techsquadeg.com>
2021-05-22 02:20:38 +0200

    add comment

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const SECRETKEY = "SECRET KEY ";
 
 const isAuth = (context) => {
+  // this is hardcoded token to test with
   context.headers.token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQsImVtYWlsIjoiYmFsc2h5QGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiYmFsc2h5IiwiaWF0IjoxNjIxMDM2MjQ2fQ.Qhz4qkkgFogUdHRo-DK_1ACVw6q4IdmRN9IRT-W8kl0";
   const token = context.headers.token;
